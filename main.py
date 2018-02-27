3330497c2a513b876b78b6bfebb4c43aba3b3cc0
劉季昀 <nefeldaiel@gmail.com>
2018-02-28 00:07:44 +0800

    Fetch image and url from leaping bunny

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import requests
 from lxml import html
 
+empty_string = "-"
+
+def get_text_from_divs(divs):
+    if len(divs) > 0:
+        return divs[0].text_content()
+    return empty_string
+
+def get_attr_from_divs(divs, attr_name):
+    if len(divs) > 0:
+        return divs[0].get(attr_name)
+    return empty_string
 
 def get_leaping_bunny():
+    # target_url = '/Users/CYLIU/Programming/Python/cruelty-free/crawler/test-data/LeapingBunny.htm'
     target_url = 'http://www.leapingbunny.org/guide/brands'
     page = requests.get(target_url)
     tree = html.fromstring(page.content)
-    divs = tree.xpath('//div[@id="lb-brands"]/div[2]/div[*]/div/div[*]/div/div[2]/span/a')
-    return [div.text_content() for div in divs]
-
+    root_div = tree.xpath('//div[@id="lb-brands"]/div[2]/div[*]/div/div[*]/div')
+    result = []
+    for item in root_div:
+        name = get_text_from_divs(item.xpath('div[2]/span/a'))
+        url = get_text_from_divs(item.xpath('div[3]/span/div/div[2]/div[1]/a'))
+        img = get_attr_from_divs(item.xpath('div[1]/div/a/img'), 'data-src')
+        result.append([name, url, img])
+    return result
 
 def get_choose_cruelty_free():
     target_url = 'http://www.choosecrueltyfree.org.au/cruelty-free-list/'
@@ -31,13 +48,16 @@ def get_choose_cruelty_free():
 # //div[@id="post-8"]/div[2]/div[1]/h4/a
 # //div[@id="post-8"]/div[2]/div[2]/h4/span/a
 
-# bunnies = get_leaping_bunny()
-# print("-- Leaping Bunny, ", len(bunnies) ,"brands -------------------------------------------------------------")
+bunnies = get_leaping_bunny()
+print("-- Leaping Bunny, ", len(bunnies) ,"brands -------------------------------------------------------------")
 # print (bunnies)
+for items in bunnies:
+    print(items)
 
-frees = get_choose_cruelty_free()
-print("-- Choose Cruelty Free, ", len(frees) ,"brands -------------------------------------------------------------")
-print(frees)
+
+# frees = get_choose_cruelty_free()
+# print("-- Choose Cruelty Free, ", len(frees) ,"brands -------------------------------------------------------------")
+# print(frees)
 # for free in frees:
 # 	print(free.text_content())
 
