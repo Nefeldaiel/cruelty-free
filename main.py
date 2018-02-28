18f16c14520798502158419bee98fd7132f1ac34
劉季昀 <nefeldaiel@gmail.com>
2018-03-01 00:20:51 +0800

    Add choose cruelty free

--- main.py
+++ main.py
@@ -53,16 +53,16 @@ def decide_type_by_img_url(img_url):
             return reg_exp[0]
     return 'Other'
 
-def page_contains_data(target_url):
+def page_contains_data(target_url, key):
     content = str(urllib2.urlopen(target_url).read())
-    return content.find('Types of product') > 0
+    return content.find(key) > 0
 
 def get_safe_shopper(target_url):
     result = []
     for page in range(0, 20):
         print('------ Page is: ' + str(page))
         url = target_url + str(page)
-        if page_contains_data(url):
+        if page_contains_data(url, 'Types of product'):
             tree = get_html_tree(url)
             root_div = tree.xpath('//section[@id="block-system-main"]/span/div/div[2]/div[1]/div[2]/div/div/div/div[2]/div[*]')
             for item in root_div:
@@ -75,16 +75,73 @@ def get_safe_shopper(target_url):
             break
     return result
 
-items = get_safe_shopper('https://www.safe.org.nz/safeshopper-cruelty-free-nz?page=')
-print("-- Safe Shopper, ", len(items) ,"brands -------------------------------------------------------------")
-# print (items)
+def can_use_rabbit_logo(class_str):
+    return str(class_str).find('pink') > -1
+
+def parse_type_from_name(name_str):
+    predefined_types = ['v', 'sv', 'vt', 'bp', 'CPOF']
+    types = []
+    splits = str(name_str).split()
+    for item in splits:
+        if item in predefined_types:
+            types.append(item)
+            predefined_types.remove(item)
+    return types
+
+
+def parse_type_and_name(name_str):
+    predefined_types = ['v', 'sv', 'vt', 'bp', 'CPOF']
+    types = []
+    name = name_str.strip()
+    try:
+        splits = name.split()
+        for item in splits:
+            if item in predefined_types:
+                types.append(item)
+                predefined_types.remove(item)
+                splits.remove(item)
+                name = ' '.join(splits)
+    except:
+        print('Fail to split ' + name)
+    return name, types
+
+
+def get_choose_cruelty_free(target_url):
+    result = []
+    for page in range(2, 3):
+        print('------ Page is: ' + str(page))
+        url = target_url + str(page)
+        if page_contains_data(url, 'Choose Cruelty Free list (a-z)'):
+            tree = get_html_tree(url)
+            name_divs = tree.xpath('//*[@id="main"]/div/div[2]/article[*]/h2/a')
+            for name_div in name_divs:
+                name, type = parse_type_and_name(name_div.text_content())
+                class_str = name_div.get('class')
+                rabbit = can_use_rabbit_logo(class_str)
+                index = name_div.get('href')
+                index = str(index).replace('#', '')
+                link_div = tree.xpath('//*[@id="' + index + '"]/div/div[1]/*/a')
+                link = get_attr_from_divs(link_div, 'href')
+                result.append([name, rabbit, link, type])
+        else:
+            break
+    return result
+
+
+items = get_choose_cruelty_free('https://choosecrueltyfree.org.au/lists/choose-cruelty-free-list/page/')
+print("-- Choose Cruelty Free, ", len(items) ,"brands -------------------------------------------------------------")
 for items in items:
     print(items)
 
 
+# items = get_safe_shopper('https://www.safe.org.nz/safeshopper-cruelty-free-nz?page=')
+# print("-- Safe Shopper, ", len(items) ,"brands -------------------------------------------------------------")
+# for items in items:
+#     print(items)
+
+
 # bunnies = get_leaping_bunny('http://www.leapingbunny.org/guide/brands')
 # print("-- Leaping Bunny, ", len(bunnies) ,"brands -------------------------------------------------------------")
-# # print (bunnies)
 # for items in bunnies:
 #     print(items)
 
